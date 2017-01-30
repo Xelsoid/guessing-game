90469b81e90296d0e5e9d8008a33a3cad4657727
xels <aerevtovich@inbox.ru>
2017-01-30 21:17:19 +0300

    Quessting-game, feature: wrote methods for game.
    Signed-off-by: xels <aerevtovich@inbox.ru>

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,46 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.quessNumber=0;
+    	this.maxRange=0;
+    	this.minRange=0;
+    }
 
     setRange(min, max) {
-
+    	this.maxRange=max;
+    	this.minRange=min;
+    	return this;
     }
 
     guess() {
+    	
+    	var start=0;
+    	var end=0;
+    	
 
+		if((this.minRange==this.maxRange-1)
+			&&(this.quessNumber<this.minRange)){
+			return this.minRange;
+		}
+		if((this.minRange==this.maxRange-1)
+			&&(this.quessNumber>this.minRange)){
+			return this.maxRange;
+		}
+		this.quessNumber=
+		Math.ceil((this.maxRange-this.minRange)/2+this.minRange);
+		return this.quessNumber;
+
+
+    	
     }
 
     lower() {
-
+    	this.maxRange=this.quessNumber;
+return this;
     }
 
     greater() {
-
+    	this.minRange=this.quessNumber;
+return this;
     }
 }
 
